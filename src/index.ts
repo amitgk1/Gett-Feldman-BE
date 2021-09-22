7973f6c163fab2be70b52aa67e78ee16c8bf91ac
Amit Genish <amitgk159753@gmail.com>
2021-09-22 15:37:40 +0300

    created an initial get route

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("initial route")
+})
 
 app.listen(5000, () => console.log("Server listening on port 5000"));
